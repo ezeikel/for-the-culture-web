c1791cf3f58cb0c97dfaeeb6ed1983087418a4b7
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-07-02 05:22:58 +0100

    Removed image component. Added bootstrap, normalize and font-awesome via node modules. Built out header component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ImageComponent } from './image/image.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
